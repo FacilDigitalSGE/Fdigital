3283d475c5d77ef858e12bc187ec33cb4f3f7d97
Silmar-Moreno <69158747+Silmar-Moreno@users.noreply.github.com>
2024-10-24 09:32:35 -0300

    Atualização do projeto

--- sistema/settings.py
+++ sistema/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'usuarios'
+    'usuarios',
+    'solicitacoes'
 ]
 
 MIDDLEWARE = [
@@ -114,14 +115,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Default primary key field type
--- sistema/urls.py
+++ sistema/urls.py
@@ -21,4 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios.urls')),
+    path('solicitacoes/', include('solicitacoes.urls')),  # inclui as rotas do app solicitacoes
+    
 ]
